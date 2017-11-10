f5ab0b4568663d3373eab228b657b5f5fffd2d94
Carolyn Lee <carolynl@uw.edu>
2017-11-10 12:34:43 -0800

    added interactive chart

--- analysis.R
+++ analysis.R
@@ -1,13 +1,10 @@
 library(dplyr)
-
 library(ggplot2)
-
 library(plotly)
+library(stringr)
 
 setwd("~/info201/a6-carolynlee1/")
 shootings.data <- read.csv("./data/shootings-2017.csv")
-
-
 
 num.shootings <- count(shootings.data)
 
@@ -16,13 +13,28 @@ num.killed <- shootings.data %>%
     sum = sum(killed)
   )
 
+num <- 5
+
 most.impacted <- shootings.data %>%
-  filter(killed > 5) %>%
+  filter(killed > num) %>%
   select(city)
 
 shootings.per.state <- shootings.data%>%
   group_by(state) %>%
   count() 
 
-
 impacted.list <- paste0(most.impacted$city)
+
+most.killed <- shootings.data %>% 
+  filter(killed == max(killed)) 
+
+killed.and.injured <- shootings.data %>%
+  group_by(state) %>%
+  summarise( 
+    sum.killed = sum(killed), 
+    sum.injured = sum(injured)
+  )
+
+s.table <- left_join(shootings.per.state, killed.and.injured)
+
+
